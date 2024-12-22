2225c85787338217bad7109a75b324e25219c575
PasanSasmika <pasansasmika198@gmail.com>
2024-12-22 18:51:51 +0530

    Create Upcomming movie database schema📑✨

---
+++ models/UpcommingMovie.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const UpcommingMovieSchema = mongoose.Schema({
+
+    movieName:{
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    Images : [
+        {
+            type : String,
+            
+        }
+    ],
+
+    releseDate:{
+        type: String,
+        required: true,
+        
+    },
+
+    trailer:{
+        type: String
+    },
+
+    description : {
+        type: String,
+        required: true
+    }
+})
+
+const UpcommingMovie = mongoose.model("commingMovie", UpcommingMovieSchema);
+
+export default  UpcommingMovie;
