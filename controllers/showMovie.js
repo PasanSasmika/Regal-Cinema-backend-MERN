9b566ef7dc68972d945386f907b8c892eb03bb92
PasanSasmika <pasansasmika198@gmail.com>
2024-12-22 17:13:48 +0530

    Create addmovie controller for add the movies and get the movies 🎬➕

---
+++ controllers/showMovie.js
@@ -0,0 +1,30 @@
+import Movie from "../models/showingMovie.js";
+
+export function AddMovie(req,res){
+
+    const  newMovieData = req.body
+
+    const movie = new Movie(newMovieData)
+
+    movie.save().then(()=>{
+        res.json({
+            message: "Movie added..!"
+        })
+    }).catch((error)=>{
+        res.json({
+            message: error
+        })
+    })
+}
+
+
+export function getMovies(req,res){
+    Movie.find({}).then((movie)=>{
+        res.json(movie)
+        
+    }).catch((error)=>{
+        res.json({
+            message:error
+        })
+    })
+}
